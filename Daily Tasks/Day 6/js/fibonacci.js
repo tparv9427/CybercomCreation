b3a4088773531217abc298cb425bd0e20e5a14b3
tparv9427 <112617905+tparv9427@users.noreply.github.com>
2026-07-15 02:48:34 +0530

    Delete Daily Tasks directory

--- Daily Tasks/Day 6/js/fibonacci.js
+++
@@ -1,5 +0,0 @@
-const FIBONACCIEBTM = document.getElementById("fibonacciBTN");
-
-const RESULT_FIBONACCI = document.getElementById("fibinacciResult");
-
-FIBONACCIEBTM.addEventListener("click", fibonacciCheck);
