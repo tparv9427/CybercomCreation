22d2f2038c3d7bf18d4dada14982a696437dd289
tparv9427 <112617905+tparv9427@users.noreply.github.com>
2026-07-15 02:49:55 +0530

    Delete practice mvc directory

--- practice mvc/app/code/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php 
-spl_autoload_register(function ($class){
-    $base = __DIR__ . "\\";
-    $file = str_replace("_", "/",$class);
-    sprintf("%s.php",$base.$file);
-});
-
-
-?>
--- practice mvc/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-include "app/Code/autoload.php";
-include "app/Sdp.php";
-Sdp::run();
-?>
